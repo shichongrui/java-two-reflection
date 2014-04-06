b8c076990956e8408027a14418a6d1ec657627e8
Matthew Sessions <hyphrend@matthew.(none)>
2014-04-06 11:33:31 -0600

    Got the slides working

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,18 +1,35 @@
-var slides = (function(window, document, undefined) {
-    var slidesClass = 'slide';
-    var buttonClass = 'nav-button';
-    var height = window.innerHeight;
-    var width = window.innerWidth;
-    
-    
+(function (window, document, $, undefined) {
+    var windowHeight = window.innerHeight;
+    var slides = null;
+    var buttons = null;
+
     function init() {
-        document.querySelectorAll('.slide');
+        slides = document.querySelectorAll(".slide");
+        buttons = document.querySelectorAll(".nav-button");
+
+        adjustSlideHeights();
+
+        attachNavEvents();
     }
-    
-    
+
+    function adjustSlideHeights() {
+        var numElements = slides.length;
+        for (var i = 0; i < numElements; i++) {
+            slides[i].style.height = windowHeight + "px";
+        }
+    }
+
+    function attachNavEvents() {
+        var numButtons = buttons.length;
+        for (var i = 0; i < numButtons; i++) {
+            buttons[i].addEventListener('click',function (e) {
+                $('html, body').animate({
+                    scrollTop: this.parentElement.nextElementSibling.offsetTop
+                },750);
+            });
+        }
+    }
+
     init();
-    
-    return {
-        
-    };
-})(window, document, undefined);
+
+})(window, document, jQuery, undefined);
