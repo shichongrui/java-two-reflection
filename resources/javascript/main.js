416e39c47778a249bc53a8122ce1f47be6fc4808
Matthew Sessions <hyphrend@matthew.(none)>
2014-04-07 19:22:21 -0600

    Got all content in and got it working in bootstrap carousel

--- resources/javascript/main.js
+++ resources/javascript/main.js
@@ -1,61 +1,29 @@
+NodeList.prototype.toArray = function() {
+    var numNodes = this.length;
+    var array = [];
+    for (var i = 0; i < numNodes; i++) {
+        array.push(this[i]);
+    }
+    return array;
+};
+
 (function (window, document, $, undefined) {
     var windowHeight = window.innerHeight;
-    var currentSlide = 1;
-    var slides = null;
-    var buttons = null;
-
+    
     function init() {
-        //scroll back up to the top
-        window.scrollTo(0,0);
+        prepCarousel();
+    }
+    
+    function prepCarousel() {
+        document.getElementById("carousel").style.height = windowHeight + "px";
+        document.querySelector(".carousel-inner").style.height = windowHeight + "px";
+        $('.carousel').carousel({
+            interval: false
+        });
         
-        slides = document.querySelectorAll(".slide");
-        buttons = document.querySelectorAll(".nav-button");
-
-        adjustSlideHeights();
-
-        attachNavEvents();
-    }
-
-    function adjustSlideHeights() {
-        var numElements = slides.length;
-        for (var i = 0; i < numElements; i++) {
-            slides[i].style.height = windowHeight + "px";
-        }
-    }
-
-    function attachNavEvents() {
-        attachButtonEvents();
-        attachKeyEvents();
-    }
-
-    function attachButtonEvents() {
-        var numButtons = buttons.length;
-        for (var i = 0; i < numButtons; i++) {
-            buttons[i].addEventListener('click', function (e) {
-                if (this.parentElement.nextElementSibling) {
-                    scrollToElement(this.parentElement.nextElementSibling);
-                    currentSlide++;
-                }
-            });
-        }
-    }
-
-    function attachKeyEvents() {
-        document.addEventListener('keydown', function (e) {
-            var key = e.keyCode ? e.keyCode : e.charCode;
-
-            if (key == 40) {
-                scrollToElement(document.getElementById('slide' + ++currentSlide));
-            } else if (key == 38) {
-                scrollToElement(document.getElementById('slide' + --currentSlide));
-            }
-        })
-    }
-
-    function scrollToElement(element) {
-        $('html, body').animate({
-            scrollTop: element.offsetTop
-        }, 750);
+        $('#carousel').on('slid', function() {
+            $('#carousel').carousel('pause');
+        });
     }
 
     init();
